cec53b8e7e8fdbd7cb0e0d8a43110682a2cd4c1f
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-06 17:33:51 -0400

    connect to database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-// require express module
+// require modules
 const express = require('express');
+const mysql = require('mysql2');
 
 // designate PORT and initialize app
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 // catchall route, so must be placed below other routes or will override
